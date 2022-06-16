53a2a7c6b19bc4d3b1f66d1f4be3eaa4a904e2ae
Thanapol Liangsoonthornsit <tliangso@z2t1c3.42bangkok.com>
2022-06-16 12:42:09 +0700

    committed ./ex00 to repo

---
+++ C05/ex00/main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tliangso <tliangso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 12:02:37 by tliangso          #+#    #+#             */
+/*   Updated: 2022/06/16 12:13:18 by tliangso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_iterative_factorial(int nb);
+
+int	main(void)
+{
+	printf("%d\n", ft_iterative_factorial(0));
+	printf("%d\n", ft_iterative_factorial(1));
+	printf("%d\n", ft_iterative_factorial(5));
+	printf("%d\n", ft_iterative_factorial(-1));
+	return (0);
+}
+
+int	ft_iterative_factorial(int nb)
+{
+	int	res;
+	int	i;
+
+	i = 1;
+	res = 1;
+	if (nb < 0)
+		return (0);
+	while (nb--)
+		res *= i++;
+	return (res);
+}
