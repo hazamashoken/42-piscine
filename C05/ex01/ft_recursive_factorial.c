4e504240ed0855f674fab762bcf876e300c05e6f
Thanapol Liangsoonthornsit <tliangso@z2t1c3.42bangkok.com>
2022-06-16 12:42:09 +0700

    committed ./ex01 to repo

---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tliangso <tliangso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 12:13:27 by tliangso          #+#    #+#             */
+/*   Updated: 2022/06/16 12:18:27 by tliangso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb >= 1)
+		return (nb * ft_recursive_factorial(nb -1));
+	else
+		return (1);
+}
---
+++ C05/ex01/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tliangso <tliangso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 12:13:27 by tliangso          #+#    #+#             */
+/*   Updated: 2022/06/16 12:18:00 by tliangso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb);
+
+int	main(void)
+{
+	printf("%d\n", ft_recursive_factorial(0));
+	printf("%d\n", ft_recursive_factorial(1));
+	printf("%d\n", ft_recursive_factorial(5));
+	printf("%d\n", ft_recursive_factorial(-1));
+	return (0);
+}
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb >= 1)
+		return (nb * ft_recursive_factorial(nb -1));
+	else
+		return (1);
+}
